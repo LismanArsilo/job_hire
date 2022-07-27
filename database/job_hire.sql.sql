00d6d1456cf0c01fe4abec54852adae6dbc71b65
lismanarsilo <lismanarsilo@gmail.com>
2022-07-27 19:45:04 +0700

    set database

--- database/job_hire.sql.sql
+++ database/job_hire.sql.sql
@@ -6,6 +6,7 @@ create table employee_range(
     emra_modified_date timestamp
 );
 -- select * from employee_range;
+
 create table client(
     clit_id integer primary key NOT NULL,
     clit_name varchar(256) NOT NULL UNIQUE,
@@ -15,6 +16,7 @@ create table client(
     clit_emra_id integer
 );
 -- select * from client;
+
 create table job_category(
     joca_id serial primary key NOT NULL,
     joca_name varchar(255),
@@ -22,12 +24,13 @@ create table job_category(
     
 );
 -- select * from job_category;
--- drop table job_category
+
 create table job_type(
     joty_id serial primary key NOT NULL,
     joty_name varchar(56)
 );
 -- select * from job_type;
+
 create table job_post(
     jopo_id serial primary key NOT NULL,
     jopo_number varchar(25) UNIQUE NOT NULL,
@@ -50,6 +53,7 @@ create table job_post(
     jopo_status varchar(15)    
 );
 -- select * from job_post;
+
 create table talent_apply(
     taap_id serial NOT NULL,
     taap_entity_id integer NOT NULL,
@@ -60,6 +64,7 @@ create table talent_apply(
     taap_status varchar(15)
 );
 -- select * from talent_apply;
+
 create table talent_apply_progress(
     tapr_id serial primary key,
     tapr_modified_date timestamp,
@@ -122,51 +127,45 @@ ALTER TABLE talent_apply_progress
 ADD FOREIGN KEY (tapr_roac_id)
 REFERENCES route_actions(roac_id) on update cascade on delete cascade;
 
+--Insert table
+INSERT INTO job_type(joty_name) VALUES ('Magang');
+INSERT INTO job_type(joty_name) VALUES ('Full-Time');
+INSERT INTO job_type(joty_name) VALUES ('Part-Time');
+INSERT INTO job_type(joty_name) VALUES ('Freelance');
+INSERT INTO job_type(joty_name) VALUES ('Contract');
+INSERT INTO job_type(joty_name) VALUES ('Prohibation');
 
-INSERT INTO job_type (joty_id, joty_name) VALUES (1, 'Magang');
-INSERT INTO job_type (joty_id, joty_name) VALUES (2, 'Full-Time');
-INSERT INTO job_type (joty_id, joty_name) VALUES (3, 'Part-Time');
-INSERT INTO job_type (joty_id, joty_name) VALUES (4, 'Freelance');
-INSERT INTO job_type (joty_id, joty_name) VALUES (5, 'Contract');
-INSERT INTO job_type (joty_id, joty_name) VALUES (6, 'Prohibation');
+INSERT INTO client(clit_id, clit_name, clit_about) 
+VALUES (1, 'Code ID', 'CodeId adalah perusahaan…');
+INSERT INTO client(clit_id, clit_name, clit_about) 
+VALUES (2, 'Telkomsel', 'CodeId adalah perusahaan…' );
+INSERT INTO client(clit_id, clit_name, clit_about) 
+VALUES (3, 'Astra International', 'CodeId adalah perusahaan…');
 
-INSERT INTO client(clit_id, clit_name, clit_about, clit_modified_date, clit_addr_id, clit_emra_id) 
-VALUES (1, 'Code ID', 'CodeId adalah perusahaan…', '', '');
-INSERT INTO client(clit_id, clit_name, clit_about, clit_modified_date, clit_addr_id, clit_emra_id) 
-VALUES (2, 'Telkomsel', 'CodeId adalah perusahaan…', '', '');
-INSERT INTO client(clit_id, clit_name, clit_about, clit_modified_date, clit_addr_id, clit_emra_id) 
-VALUES (3, 'Astra International', 'CodeId adalah perusahaan…', '', '');
+INSERT INTO job_category(joca_name) VALUES ('Software Engineer');
+INSERT INTO job_category(joca_name) VALUES ('Business Development');
+INSERT INTO job_category(joca_name) VALUES ('Marketing');
+INSERT INTO job_category(joca_name) VALUES ('Operations');
 
-INSERT INTO job_category(joca_id, joca_name) VALUES (1, 'Software Engineer');
-INSERT INTO job_category(joca_id, joca_name) VALUES (2, 'Business Development');
-INSERT INTO job_category(joca_id, joca_name) VALUES (3, 'Marketing');
-INSERT INTO job_category(joca_id, joca_name) VALUES (4, 'Operations');
+INSERT INTO job_post(jopo_number, jopo_title, jopo_min_salary, jopo_max_salary, jopo_description,
+					 jopo_responsibility, jopo_target, jopo_benefit, jopo_start_date, jopo_end_date,
+					 jopo_emp_entity_id, jopo_clit_id, jopo_joro_id, jopo_joty_id, jopo_joca_id, 
+					 jopo_status) 
+VALUES ('JOB20220727-0001', 'Java Developer', 9000000, 16000000, 
+		'{"item" : "Developing, implementing, and maintaining Java based components and interfaces. Coordinate with the rest of the team working on different layers of the infrastructure. Delivering code with well tested"}', 
+		'{"items" : "Proficient in Core Java, with a solid understanding of object-oriented programming Have minimum 1 year of working experience in Java Programming Skill Set: Spring framework, JPA / Hibernate Experience with database Oracle, My SQL, Postgre SQL or MS SQL Server Able to working with Agile methodology Can join immediately is a plus" }',
+		'{"item" : "Sertifikat Professional, D3 (Diploma), D4 (Diploma), Sarjana (S1),Diploma Pascasarjana, Gelar Professional, Magister (S2)"}',
+		'{"item": "Career growth in software development Positive working environment Great opportunity to get experiences in several industry sectors"}',
+		'12-Jul-22', '12-Aug-22', 7, 3, 1, 5, 1, 'publish');
 
-INSERT INTO job_post(jopo_id, jopo_number, jopo_title, jopo_min_salary, jopo_max_salary, jopo_description, jopo_responsibility, jopo_target, jopo_benefit, jopo_start_date, jopo_end_date, jopo_emp_entity_id, jopo_clit_id, jopo_joro_id, jopo_joty_id, jopo_joca_id, jopo_status) 
-VALUES (1, 'JOB20220727-0001', 'Java Developer', 9,000,000, 16,000,000, '{
-"item" : "Developing, implementing, and maintaining Java based components and interfaces.
-Coordinate with the rest of the team working on different layers of the infrastructure.
-Delivering code with well tested"}', '{
-"items":"Proficient in Core Java, with a solid understanding of object-oriented programming
-Have minimum 1 year of working experience in Java Programming
-Skill Set: Spring framework, JPA / Hibernate
-Experience with database Oracle, My SQL, Postgre SQL or MS SQL Server
-Able to working with Agile methodology
-Can join immediately is a plus"
-}', '{"item":"
-Sertifikat Professional, D3 (Diploma), D4 (Diploma), Sarjana (S1), Diploma Pascasarjana, Gelar Professional, Magister (S2)
-"}', '{"item"":""Career growth in software development
-Positive working environment
-Great opportunity to get experiences in several industry sectors"}', '12-Jul-22', '12-Aug-22', 7, 3, 1, 5, 1, 'publish');
+INSERT INTO talent_apply(taap_entity_id, taap_jopo_id, taap_intro, taap_scoring, taap_status)
+VALUES (9, 1, 'perkenalkan nama saya andhika, saya data scienties anthusias', 8, 'interview');
 
-INSERT INTO talent_apply(taap_id, taap_entity_id, taap_jopo_id, taap_intro, taap_scoring, taap_status)
-VALUES (1, 9, 1, 'perkenalkan nama saya andhika, saya data scienties anthusias', 8, 'interview');
-
-INSERT INTO talent_apply_progress(tapr_id, tapr_modified_date, tapr_status, tapr_comment, tapr_entity_id , tapr_roac_id)
-VALUES (1, '', 'done', '', 9, 10); 
-INSERT INTO talent_apply_progress(tapr_id, tapr_modified_date, tapr_status, tapr_comment, tapr_entity_id , tapr_roac_id)
-VALUES (2, '', 'done', '', 9, 11); 
-INSERT INTO talent_apply_progress(tapr_id, tapr_modified_date, tapr_status, tapr_comment, tapr_entity_id , tapr_roac_id)
-VALUES (3, '', 'waiting', '', 9, 12); 
-INSERT INTO talent_apply_progress(tapr_id, tapr_modified_date, tapr_status, tapr_comment, tapr_entity_id , tapr_roac_id)
-VALUES (4, '', 'waiting', '', 9, 14); 
+INSERT INTO talent_apply_progress(tapr_status, tapr_entity_id , tapr_roac_id)
+VALUES ('done', 9, 10); 
+INSERT INTO talent_apply_progress(tapr_status, tapr_entity_id , tapr_roac_id)
+VALUES ('done', 9, 11); 
+INSERT INTO talent_apply_progress(tapr_status, tapr_entity_id , tapr_roac_id)
+VALUES ('waiting', 9, 12); 
+INSERT INTO talent_apply_progress(tapr_status, tapr_entity_id , tapr_roac_id)
+VALUES ('waiting', 9, 14); 
